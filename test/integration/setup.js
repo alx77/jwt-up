860920b859120be3650a95136bea20e9ae62d8c9
Alexey Furmanov <a.furmanoff@gmail.com>
2026-01-04 21:16:48 +0100

    JUP-2 try to run integration test from docker image

--- test/integration/setup.js
+++ test/integration/setup.js
@@ -1,18 +1,21 @@
 import { beforeAll } from "vitest";
 import waitOn from "wait-on";
+import cfg from "@/common/config.js";
 
 beforeAll(async () => {
   try {
     console.log("⏳ Waiting for PostgreSQL...");
+    const url = new URL(cfg.get("POSTGRES_CONN"));
+
     await waitOn({
-      resources: ["tcp:localhost:25432"],
+      resources: [`tcp:${url.hostname}:${url.port}`],
       timeout: 60000,
       interval: 1000,
     });
 
     console.log("⏳ Waiting for Redis...");
     await waitOn({
-      resources: ["tcp:localhost:26379"],
+      resources: [`tcp:${cfg.get("REDIS_HOST")}:${cfg.get("REDIS_PORT")}`],
       timeout: 40000,
       interval: 1000,
     });
